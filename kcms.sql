26907e964396077d8386fcaaa5924f8183d5a695
depchuanseo <depchuanseo@gmail.com>
2014-10-28 17:04:50 +0700

    commit sql

---
+++ kcms.sql
@@ -0,0 +1,233 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.20 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             8.3.0.4694
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table kcms.albums
+CREATE TABLE IF NOT EXISTS `albums` (
+  `id` bigint(20) NOT NULL,
+  `category_id` bigint(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  `meta_title` varchar(70) DEFAULT NULL,
+  `meta_keywords` varchar(255) DEFAULT NULL,
+  `meta_description` varchar(160) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.albums: ~0 rows (approximately)
+/*!40000 ALTER TABLE `albums` DISABLE KEYS */;
+/*!40000 ALTER TABLE `albums` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.albumsimages
+CREATE TABLE IF NOT EXISTS `albumsimages` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `model` varchar(20) NOT NULL,
+  `foreign_key` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `attachment` varchar(255) NOT NULL,
+  `dir` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `size` int(11) DEFAULT '0',
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.albumsimages: ~0 rows (approximately)
+/*!40000 ALTER TABLE `albumsimages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `albumsimages` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.banners
+CREATE TABLE IF NOT EXISTS `banners` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `terms` varchar(50) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `description` text,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  `params` text,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.banners: ~0 rows (approximately)
+/*!40000 ALTER TABLE `banners` DISABLE KEYS */;
+/*!40000 ALTER TABLE `banners` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `image` varchar(50) DEFAULT NULL,
+  `description` text,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  `meta_title` varchar(70) DEFAULT NULL,
+  `meta_keywords` varchar(255) DEFAULT NULL,
+  `meta_description` varchar(160) DEFAULT NULL,
+  `template` varchar(50) DEFAULT NULL,
+  `terms` varchar(50) DEFAULT 'post',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.categories: ~1 rows (approximately)
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` (`id`, `title`, `slug`, `image`, `description`, `published`, `meta_title`, `meta_keywords`, `meta_description`, `template`, `terms`, `created`, `modified`) VALUES
+	(1, 'Uncategorized', 'uncategorized', NULL, NULL, 1, NULL, NULL, NULL, NULL, 'page', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.groups
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `slug` varchar(50) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.groups: ~3 rows (approximately)
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` (`id`, `title`, `slug`, `created`, `modified`) VALUES
+	(1, 'Admin', 'admin', '2014-10-28 13:37:11', '2014-10-28 13:37:12'),
+	(2, 'Manage', 'manage', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(3, 'Member', 'member', '2014-10-28 13:37:49', '2014-10-28 13:37:51');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.links
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.links: ~0 rows (approximately)
+/*!40000 ALTER TABLE `links` DISABLE KEYS */;
+/*!40000 ALTER TABLE `links` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.menus
+CREATE TABLE IF NOT EXISTS `menus` (
+  `id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.menus: ~0 rows (approximately)
+/*!40000 ALTER TABLE `menus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menus` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.posts
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `description` longtext,
+  `meta_title` varchar(70) DEFAULT NULL,
+  `meta_keywords` varchar(255) DEFAULT NULL,
+  `meta_description` varchar(160) DEFAULT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  `terms` varchar(50) NOT NULL DEFAULT 'post',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.posts: ~0 rows (approximately)
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.products
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `price` decimal(10,2) DEFAULT '0.00',
+  `price_sales` decimal(10,2) DEFAULT '0.00',
+  `description` longtext,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  `meta_title` varchar(70) DEFAULT NULL,
+  `meta_keywords` varchar(255) DEFAULT NULL,
+  `meta_description` varchar(160) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.products: ~0 rows (approximately)
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.productsimages
+CREATE TABLE IF NOT EXISTS `productsimages` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `model` varchar(20) NOT NULL,
+  `foreign_key` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `attachment` varchar(255) NOT NULL,
+  `dir` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `size` int(11) DEFAULT '0',
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.productsimages: ~0 rows (approximately)
+/*!40000 ALTER TABLE `productsimages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productsimages` ENABLE KEYS */;
+
+
+-- Dumping structure for table kcms.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table kcms.users: ~1 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `group_id`, `username`, `password`, `email`, `published`, `created`, `modified`) VALUES
+	(1, 3, 'admin', '$2a$10$7X2llSRxP6CrC8fzIKpMfe4NnjVkozqeYnLAJeS2DxpuQN4QFvKwe', 'depchuanseo@gmail.com', 1, '2014-10-28 09:32:15', '2014-10-28 09:32:15');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
